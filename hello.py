e6dd2907c03429f4b90eb47783544e41ac8aa87a
Константин Манаков <konstantinmanakov@MacBook-Pro-Konstantin.local>
2020-05-04 13:09:06 +0500

    step 1.9

---
+++ hello.py
@@ -0,0 +1,10 @@
+def wsgi_application(environ, start_response):
+    status = "200 OK"
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    data = environ['QUERY_STRING'].split('&')
+    body = '\n'.join(data).encode('utf-8')
+    start_response(status, headers)
+    return [body]
+
