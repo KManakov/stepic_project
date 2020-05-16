70950ae81a58e8c8e5eefbec006aac10ef41bacd
Константин Манаков <konstantinmanakov@MacBook-Pro-Konstantin.local>
2020-05-16 17:56:01 +0500

    step 2.1 attempt 1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,13 +4,13 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^popular/.*$', views.popular, name='popular'),    
-    url(r'^ask/.*$', views.ask, name='ask'),
-    url(r'^answer/.*$', views.answer, name='answer'),
-    url(r'^signup/.*$', views.user_signup, name='signup'),
-    url(r'^login/.*$', views.user_login, name='login'),
-    url(r'^logout/.*$', views.user_logout, name='logout'),    
-    url(r'^new/.*$', views.test),
-    url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),    
+    url(r'^$', name='index'),
+    url(r'^popular/.*$', name='popular'),    
+    url(r'^ask/.*$', name='ask'),
+    url(r'^answer/.*$', name='answer'),
+    url(r'^signup/.*$', name='signup'),
+    url(r'^login/.*$', name='login'),
+    url(r'^logout/.*$', name='logout'),    
+    url(r'^new/.*$'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', name='question'),    
 ]
