e69e5df0616a00528098b887d8cfc61e78378a46
Константин Манаков <konstantinmanakov@MacBook-Pro-Konstantin.local>
2020-05-17 18:26:17 +0500

    step 2.1 attempt 2

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test, name='test'),
-    url(r'^login/.*$', views.test),
-    url(r'^signup/.*$', views.test),
-    url(r'^ask/.*$', views.test),
-    url(r'^popular/.*$', views.test),
-    url(r'^new/.*$', views.test),
-    url(r'^question/(?P<question_id>[0-9]+)/$', views.test),
+    url(r'^login/', views.test),
+    url(r'^signup/', views.test),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.test),
+    url(r'^new/', views.test),
+    url(r'^question/(?P<question_id>[0-9]+)/', views.test),
 ]
